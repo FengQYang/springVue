23e8ce2512c5605447a2a0821e66afcd3a056185
78520 <1468416647@qq.com>
2020-08-19 08:56:24 +0800

    springboot-vue工程

--- src/main/java/com/fqy/controller/UserController.java
+++ src/main/java/com/fqy/controller/UserController.java
@@ -3,12 +3,14 @@ package com.fqy.controller;
 import com.fqy.pojo.User;
 import com.fqy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
     @Autowired
